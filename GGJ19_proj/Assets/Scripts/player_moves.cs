fb5d5d114bf79729f8013a9dcb04da33fc7890cf
Alexandre Chícharo <chicharo.alexandre@gmail.com>
2019-01-25 22:12:14 +0000

    added prefab

--- GGJ19_proj/Assets/Scripts/player_moves.cs
+++ GGJ19_proj/Assets/Scripts/player_moves.cs
@@ -1,39 +1,30 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class player_moves : MonoBehaviour
-{
-    
-    public float speed;             //Floating point variable to store the player's movement speed.
+public class player_moves : MonoBehaviour {
+    public float speed; //Floating point variable to store the player's movement speed.
 
-    private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
-    
+    private Rigidbody2D rb2d; //Store a reference to the Rigidbody2D component required to use 2D Physics.
+
     // Start is called before the first frame update
-    void Start()
-    {
-        rb2d = GetComponent<Rigidbody2D> ();
+    void Start() {
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-
+    void Update() {
         string key = Input.inputString;
-        if(key.Length>0)
+        if (key.Length > 0)
             Debug.Log(key);
         //Store the current horizontal input in the float moveHorizontal.
-        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveHorizontal = Input.GetAxis("Horizontal");
 
         //Store the current vertical input in the float moveVertical.
-        float moveVertical = Input.GetAxis ("Vertical");
+        float moveVertical = Input.GetAxis("Vertical");
 
         //Use the two store floats to create a new Vector2 variable movement.
-        Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
 
         //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
-        rb2d.AddForce (movement * speed);
-        
+        rb2d.AddForce(movement * speed);
     }
 }
