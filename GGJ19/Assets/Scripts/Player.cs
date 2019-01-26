be8b7119cfb36616e79173cb0823187dc4bafd12
NMBLM <nuno.lages.martins@tecnico.ulisboa.pt>
2019-01-26 11:12:04 +0000

    Dog animation and follow, Sprint and stamina

--- GGJ19/Assets/Scripts/Player.cs
+++ GGJ19/Assets/Scripts/Player.cs
@@ -1,13 +1,14 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Player : MonoBehaviour {
-    
     private Rigidbody2D rigid;
     private Animator animator;
     private Vector2 moveDirection = Vector2.zero;
     public float speed = 6.0f;
     public float sprintSpeed = 12.0f;
-    
+    public float stamina = 100.0f;
+
     private void Start() {
         rigid = GetComponent<Rigidbody2D>();
         animator = GetComponentInChildren<Animator>();
@@ -16,15 +17,15 @@ public class Player : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-
         move();
         //Debug.Log("mov_f: " + moveDirection);
     }
 
     private void move() {
-        
         var currSpeed = 0.0f;
-        
+        var decay = 20.0f;
+        var restore = 15.0f;
+
         if (Input.GetKeyDown("up") || Input.GetKeyDown("w")) {
             moveDirection = new Vector2(0.0f, 1.0f);
             playAnim("PlayerMoveUp");
@@ -46,21 +47,36 @@ public class Player : MonoBehaviour {
         }
 
         if (Input.GetKey("up") || Input.GetKey("w") || Input.GetKey("left") || Input.GetKey("a")
-            ||Input.GetKey("down") || Input.GetKey("s") || Input.GetKey("right") || Input.GetKey("d")) {
+            || Input.GetKey("down") || Input.GetKey("s") || Input.GetKey("right") || Input.GetKey("d")) {
             currSpeed = speed;
         }
-        
+        else {
+            moveDirection = Vector2.zero;
+        }
+
         if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) {
             currSpeed = sprintSpeed;
+            if (moveDirection != Vector2.zero) {
+                stamina -= decay * Time.deltaTime;
+            }
+
+            if (stamina <= 0.0f) {
+                stamina = -1.0f;
+                currSpeed = speed;
+            }
         }
 
         var endMoveDirection = moveDirection * currSpeed;
-
         if (endMoveDirection == Vector2.zero) {
             playAnim("PlayerIdle");
+            stamina += restore * Time.deltaTime;
+            if (stamina > 100.0f) {
+                stamina = 100.0f;
+            }
         }
-        
-        rigid.MovePosition(rigid.position +  endMoveDirection * Time.deltaTime);
+
+        animator.speed = currSpeed / speed;
+        rigid.MovePosition(rigid.position + endMoveDirection * Time.deltaTime);
     }
 
     private void playAnim(string animName) {
