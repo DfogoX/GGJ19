5902297f245ad74bf8c98d15c09d0d2e288b83d3
DfogoX <diogocariaferreira@gmail.com>
2019-01-26 19:00:47 +0000

    player damage and immunity

--- GGJ19/Assets/Scripts/Player.cs
+++ GGJ19/Assets/Scripts/Player.cs
@@ -1,21 +1,29 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+    
     private Rigidbody2D rigid;
     private Animator animator;
+    private SpriteRenderer rend;
     private Vector2 moveDirection = Vector2.zero;
+    private float cd_damage = 0.5f;
+    
     public float speed = 6.0f;
     public float sprintSpeed = 12.0f;
     public float stamina = 100.0f;
     public float moveX = 0f;
     public float moveY = 0f;
-    public int playerHP = 5;
+    private int playerHP = 5;
+    private bool immune;
 
     private void Start() {
+        immune = false;
         GameManager.GM.setPlayer(this);
         rigid = GetComponent<Rigidbody2D>();
         animator = GetComponentInChildren<Animator>();
+        rend = GetComponentInChildren<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -25,6 +33,7 @@ public class Player : MonoBehaviour {
         spawning();
 
         damaging();
+
     }
 
     private void move() {
@@ -136,14 +145,14 @@ public class Player : MonoBehaviour {
     private void playAnim(string animName) {
         animator.Play(animName);
     }
-
+//SPAWNING
     private void spawning() {
         if (Input.GetKey(KeyCode.K))
             GameManager.GM.activateSpawner();
         if (Input.GetKey(KeyCode.L))
             GameManager.GM.deactivateSpawner();
     }
-
+//DAMAGING
     private void damaging() {
         if (Input.GetKeyDown(KeyCode.C)) {
             GameManager.GM.damagePlayer(1);
@@ -158,12 +167,30 @@ public class Player : MonoBehaviour {
             GameManager.GM.healPlayer(1);
             Debug.Log("hp: " + playerHP);
         }
+
+        cd_damage = 3.0f;
+
     }
 
-    public void takeDamage(int amount) {
-        playerHP -= amount;
+    public bool takeDamage(int amount) {
+        if (!immune) {
+            Debug.Log("entrou");
+            rend.color = Color.red;
+            playerHP -= amount;
+            immune = true;
+            StartCoroutine(ImmunityCooldown());
+            return true;
+        }
+
+        return false;
     }
-    
+
+    private IEnumerator ImmunityCooldown() {
+        yield return new WaitForSeconds(cd_damage);
+        immune = false;
+        rend.color = Color.white;
+    }
+
     public void giveHeal(int amount) {
         playerHP += amount;
     }
