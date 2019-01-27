9a71f0a6db841856ca83bbacc554f244e962a5d2
DfogoX <diogocariaferreira@gmail.com>
2019-01-27 04:05:33 +0000

    grid bom, com efeito de heal

--- GGJ19/Assets/House.cs
+++ GGJ19/Assets/House.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class House : MonoBehaviour
-{
+public class House : MonoBehaviour {
+
+    private float cd_heal;
+    private bool inside;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,12 +15,24 @@ public class House : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (inside) {
+            GameManager.GM.healPlayer(1);
+        }
         
     }
-    
+
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Player")) {
+            GameManager.GM.activateSpawner();
+            inside = true;
+        }
+    }
+
     private void OnTriggerExit2D(Collider2D other) {
         if (other.gameObject.CompareTag("Player")) {
             GameManager.GM.activateSpawner();
-            
+            inside = false;
         }
+    }
 }
